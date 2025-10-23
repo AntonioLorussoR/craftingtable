fa216342278f0bb9221b4e8000437cb2d5342a35
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-10-23 16:21:29 +0200

    Update OAuthSuccess.jsx

--- src/components/OAuthSuccess.jsx
+++ src/components/OAuthSuccess.jsx
@@ -7,10 +7,10 @@ export default function OAuthSuccess({ onLogin }) {
 
   useEffect(() => {
     const token = params.get("token");
-    if (token) {
+    if (token && typeof window !== "undefined") {
       localStorage.setItem("token", token);
       onLogin?.(token);
-      navigate("/dashboard", { replace: true }); //vai direttamente alla dashboard
+      navigate("/dashboard", { replace: true });
     } else {
       navigate("/", { replace: true }); // fallback se manca il token
     }
