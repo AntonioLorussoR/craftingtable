dc85f881e44a7c8a531ebe161095c10eae75921c
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-10-23 16:11:29 +0200

    Update Other.jsx

--- src/pages/Team/TeamContent/Other.jsx
+++ src/pages/Team/TeamContent/Other.jsx
@@ -9,11 +9,13 @@ export default function Other({ team, token }) {
   const API_BASE = import.meta.env.VITE_API_BASE_URL;
   
   let currentUserId = null;
-  try {
-    const decoded = jwtDecode(token);
-    currentUserId = decoded._id || decoded.id || decoded.userId || decoded.sub;
-  } catch (err) {
-    console.warn("Token non valido:", err);
+  if (token) {
+    try {
+      const decoded = jwtDecode(token);
+      currentUserId = decoded._id || decoded.id || decoded.userId || decoded.sub;
+    } catch (err) {
+      console.warn("Token non valido:", err);
+    }
   }
 
   const isAdmin = team?.members?.some(
