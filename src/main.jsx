3f596c0d07f3a616f5508e63619a68c1f75fb387
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-10-23 15:10:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { io } from "socket.io-client";
 
 
-export const socket = io("http://localhost:5000", {
+export const socket = io(import.meta.env.VITE_API_BASE_URL, {
   auth: { token: localStorage.getItem("token") },
 });
 
