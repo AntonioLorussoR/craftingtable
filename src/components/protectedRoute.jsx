de2ffd4623b0e7e6891acb1f60d93daea0b73656
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-11-02 12:36:04 +0100

    Update protectedRoute.jsx

--- src/components/protectedRoute.jsx
+++ src/components/protectedRoute.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
-export default function ProtectedRoute({ token, children }) {
+export default function ProtectedRoute({ children }) {
+  const { token } = useContext(AuthContext);
   const location = useLocation();
 
   if (!token && location.pathname !== "/") {
