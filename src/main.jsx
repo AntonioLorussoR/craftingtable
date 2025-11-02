a0bf72cc994271286a85f251baa72026b9c4c434
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-11-02 11:36:56 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { io } from "socket.io-client";
-
-
-export const socket = io(import.meta.env.VITE_API_BASE_URL, {
-  transports: ["websocket", "polling"],
-  withCredentials: true,
-  auth: { token: localStorage.getItem("token") },
-});
-
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
